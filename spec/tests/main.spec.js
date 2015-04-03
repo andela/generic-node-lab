5317fa4af47a154e4f998f0bad7b1a790b0b978f
andela-gukpere <godson.ukpere@andela.co>
2015-04-03 12:17:37 +0100

    Initial Commit

---
+++ spec/tests/main.spec.js
@@ -0,0 +1,9 @@
+describe('Mongo ODM Lab', function() {
+  it('should be witty', function() {
+      expect(1).toBe(1);
+  });
+
+  it('should be a pitty', function() {
+      expect(1).toBe(1);
+  });
+});
